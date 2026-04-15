3545250ff36cc3c76048ce5f4062cf9cc5a66643
pingu543 <pcu50208@outlook.com>
2026-04-15 12:57:17 -0400

    Add project files.

---
+++ NeighborhoodContacts.Server/Data/Entities/Property.cs
@@ -0,0 +1,9 @@
+﻿namespace NeighborhoodContacts.Server.Data.Entities
+{
+    public class Property
+    {
+        public Guid Id { get; set; }
+        public string Address { get; set; } = string.Empty;
+        public List<User> Users { get; set; } = [];
+    }
+}
---
+++ NeighborhoodContacts.Server/Data/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿namespace NeighborhoodContacts.Server.Data.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public Guid? PropertyId { get; set; }
+        public Property? Property { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Hash { get; set; } = string.Empty;
+        public string Salt { get; set; } = string.Empty;
+        public string ContactName { get; set; } = string.Empty;
+        public string? ContactNumber { get; set; }
+        public string? ContactEmail { get; set; }
+        public string? SelfIntroduction { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsAdmin { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
